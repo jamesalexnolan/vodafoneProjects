85d70660372a05b26953db2d92b4c8e26ef6a092
James Nolan <87645203+jamesalexnolan@users.noreply.github.com>
2024-12-05 11:46:08 +0000

    Delete Vodafone/Vodafone directory

--- Vodafone/Vodafone/return target build titles.js
+++
@@ -1,8 +0,0 @@
-var elements = document.querySelectorAll('.spectrum-Link--primary')
-var array = []
-
-elements.forEach(function(element){
-    array.push(element.innerText)
-})
-
-console.log(array)
